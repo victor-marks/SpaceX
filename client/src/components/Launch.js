f592349602d35712fe60ed48e485af7f1357ffd8
Victor Marks <vjmarks@gmail.com>
2019-07-10 15:44:31 -0700

    added routing for each mission

---
+++ client/src/components/Launch.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+export class Launch extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Launch</h1>
+      </div>
+    );
+  }
+}
+
+export default Launch;
